6449d94e230262e1d28c802581acd8de08f0cebd
nguyenvantoan1821051127 <79552710+nguyenvantoan1821051127@users.noreply.github.com>
2024-06-30 23:59:01 +0700

    first commit

--- servers/src/services/vitritinhthanh.js
+++ servers/src/services/vitritinhthanh.js
@@ -16,3 +16,28 @@ export const getAllViTriService = ()=> new Promise(async(resolve,reject)=>{
     }
 })
 
+export const getNewVitriService = () => new Promise(async (resolve, reject) => {
+    try {
+        const response = await db.ViTriTinhThanh.findOrCreate({
+            raw: true,
+            nest: true,
+            offset: 0,
+            // order: [['createdAt', 'DESC']],
+            // limit: +process.env.LIMIT,
+            // include: [
+            //     { model: db.Image, as: 'images', attributes: ['image'] },
+            //     { model: db.Attribute, as: 'attributes', attributes: ['price', 'acreage', 'published', 'hashtag'] },
+            // ],
+            // attributes: ['id', 'title', 'star', 'createdAt']
+        })
+        resolve({
+            err: response ? 0 : 1,
+            msg: response ? 'OK' : 'Getting posts is failed.',
+            response
+        })
+
+    } catch (error) {
+        reject(error)
+    }
+})
+
